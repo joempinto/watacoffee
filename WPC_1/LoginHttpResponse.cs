ff2396aa563176838296c097b8e068e4ac53372b
joempinto <goodpinto@gmail.com>
2023-10-14 18:48:19 +0200

    Added login logic

---
+++ WPC_1/LoginHttpResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC_1
+{
+    internal struct LoginHttpResponse
+    {
+        private string Email {  get; set; } //Capital 'E' due server response variables names
+        private string Token { get; set; }
+        private string Prefix { get; set; }
+
+        public LoginHttpResponse(in string email, in string token,in string prefix)
+        {
+            this.Email = email;
+            this.Token = token;
+            this.Prefix = prefix;   
+        }
+
+        public static implicit operator Task<object>(LoginHttpResponse v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
